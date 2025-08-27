154e3f97a726e06b50dffdbeb54fc64bd9b007a5
EzeAssenza12 <168022849+EzeAssenza12@users.noreply.github.com>
2025-08-27 17:01:58 -0300

    Primer commit

--- Sysacad-main/app/mapping/area_mapping.py
+++ Sysacad-main/app/mapping/area_mapping.py
@@ -3,7 +3,7 @@ from app.models import Area
 
 class AreaMapping(Schema):
     id = fields.Integer(dump_only=True)
-    # pyrefly: ignore  # bad-argument-type
+
     nombre = fields.String(required=True, validate=validate.Length(min=1, max=50))
     
     @post_load
